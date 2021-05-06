f5a8b29eba6433002e5a5741cd5b750e98e19a33
Net Zhang <zhang.11091@osu.edu>
2021-05-06 16:00:56 -0400

    Tracing Plot updates

--- assist/banditProduction.R
+++ assist/banditProduction.R
@@ -5,21 +5,23 @@ N_BANDIT <- 10
 init_param_logs <- function(n_bandit = N_BANDIT) {
   param_logs <- list(
     alpha = tibble(
-      bandit = paste0("alpha_", c(1:n_bandit)),
-      init = 1
-    ) %>%
-      pivot_wider(
-        bandit, names_from = bandit, 
-        values_from = init
-      ),
-    beta = tibble(
-      bandit = paste0("beta_", c(1:n_bandit)),
-      init = 1
-    ) %>%
-      pivot_wider(
-        bandit, names_from = bandit, 
-        values_from = init
+      step = 1,
+      param = list(
+        tibble(
+          bandit = paste0("alpha_", c(1:n_bandit)),
+          value = 1
+        )
       )
+    ),
+    beta = tibble(
+      step = 1,
+      param = list(
+        tibble(
+          bandit = paste0("beta_", c(1:n_bandit)),
+          value = 1
+        )
+      )
+    )
   )
   return(param_logs)
 }
@@ -42,41 +44,46 @@ site_selection_logs <- init_site_selection_logs(n_bandit = N_BANDIT)
 
 tps_sampling_recommendation <- function(param_logs) {
   # Extract the most recent posterior ------------------------------
-  alpha_post <- param_logs$alpha %>% tail(1)
-  beta_post <- param_logs$beta %>% tail(1)
-  n_bandit <- ncol(alpha_post)
+  alpha_post <- param_logs$alpha %>%
+    arrange(step) %>%
+    tail(1) %>%
+    select(param) %>%
+    unnest(cols = c(param))
+  beta_post <- param_logs$beta %>%
+    arrange(step) %>%
+    tail(1) %>%
+    select(param) %>%
+    unnest(cols = c(param))
   
-  aggregate_distribution <- 
+  n_bandit <- nrow(beta_post)
+  
+  posterior_distribution <- 
     tibble(
       bandit = paste0("banit_", c(1:n_bandit))
     ) %>%
     bind_cols(
       alpha_post %>%
-        pivot_longer(everything(), 
-                     names_to = "param", values_to = "alpha") %>%
-        select(alpha),
+        select(alpha = value),
       beta_post %>%
-        pivot_longer(everything(), 
-                     names_to = "param", values_to = "beta") %>%
-        select(beta)
+        select(beta = value)
     )
   
   # Pull Random Sample from posterior distribution ----------------
-  aggregate_distribution <- aggregate_distribution %>%
+  posterior_distribution <- posterior_distribution %>%
     mutate(
       bandit_id = row_number(),
-      p = aggregate_distribution %>%
+      p = posterior_distribution %>%
         select(
           shape1 = alpha, shape2 = beta
         ) %>% pmap_dbl(rbeta, n = 1)
     )
-  return(aggregate_distribution)
+  return(posterior_distribution)
 }
 
-peek_top_recommendation <- function(aggregate_distribution, top_n = 5) {
+peek_top_recommendation <- function(posterior_distribution, top_n = 5) {
   
   return(
-    aggregate_distribution %>%
+    posterior_distribution %>%
     arrange(-p) %>%
     head(top_n)
   )
@@ -94,68 +101,113 @@ peek_top_recommendation <- function(aggregate_distribution, top_n = 5) {
 
 tidy_testing_data <- function(testing_data) {}
 
-update_posterior <- function(testing_data, param_logs, site_selection_logs) {
+update_posterior <- function(testing_data, param_logs) {
+   
+   # Latest Parameters
+   current_step <- param_logs$alpha %>%
+     arrange(step) %>%
+     tail(1) %>%
+     pull(step)
+   latest_alpha <- param_logs$alpha %>%
+     arrange(step) %>%
+     tail(1) %>%
+     select(param) %>%
+     unnest(cols = param)
+   
+   latest_beta <- param_logs$beta %>%
+     arrange(step) %>%
+     tail(1) %>%
+     select(param) %>%
+     unnest(cols = param)
    
    # Update Alpha
-   alpha_names <- names(param_logs$alpha)
-   
    updated_alpha <- 
-     param_logs$alpha %>%
-     tail(1) %>%
-     pivot_longer(everything(), 
-                  values_to = "alpha",
-                  names_to = "name") %>%
-     transmute(
-       bandit = parse_number(name),
-       alpha
-     ) %>%
+     latest_alpha %>%
      bind_rows(
        testing_data %>%
-         select(bandit, alpha)
+         transmute(
+           bandit = paste0("alpha_", bandit),
+           value = alpha
+         )
      ) %>%
      group_by(bandit) %>%
-     summarise_at(vars(alpha), .funs = list(sum)) %>%
-     arrange(bandit) %>% 
-     pivot_wider(values_from = alpha, 
-                 names_from = bandit) %>%
-     set_names(alpha_names)
-    param_logs$alpha <- param_logs$alpha %>%
-      bind_rows(
-        updated_alpha
-      )
-    assign("param_logs", param_logs, envir = .GlobalEnv)
-   # Update Beta
+     summarise_at(vars(value), .funs = list(sum)) %>%
+     mutate(
+       bandit_id = parse_number(bandit)
+     ) %>%
+     arrange(bandit_id) %>%
+     select(-bandit_id)
    
-   beta_names <- names(param_logs$beta)
-   
+   param_logs$alpha <- param_logs$alpha %>%
+     bind_rows(
+       tibble(
+         step = current_step + 1,
+         param = list(updated_alpha)
+       )
+     )
+   # Update Beta 
    updated_beta <- 
-     param_logs$beta %>%
-     tail(1) %>%
-     pivot_longer(everything(), 
-                  values_to = "beta",
-                  names_to = "name") %>%
-     transmute(
-       bandit = parse_number(name),
-       beta
-     ) %>%
+     latest_beta %>%
      bind_rows(
        testing_data %>%
-         select(bandit, beta)
+         transmute(
+           bandit = paste0("beta_", bandit),
+           value = beta
+         )
      ) %>%
      group_by(bandit) %>%
-     summarise_at(vars(beta), .funs = list(sum)) %>%
-     arrange(bandit) %>% 
-     pivot_wider(values_from = beta, 
-                 names_from = bandit) %>%
-     set_names(beta_names)
+     summarise_at(vars(value), .funs = list(sum)) %>%
+     mutate(
+       bandit_id = parse_number(bandit)
+     ) %>%
+     arrange(bandit_id) %>%
+     select(-bandit_id)
+   
    param_logs$beta <- param_logs$beta %>%
      bind_rows(
-       updated_beta
-     )
-   assign("param_logs", param_logs, envir = .GlobalEnv)
+       tibble(
+         step = current_step + 1,
+         param = list(updated_beta)
+        )
+      )
+    assign("param_logs", param_logs, envir = .GlobalEnv)
 }
 
-
+trace_plot <- function(param_logs, bandit = 1) {
+  
+  get_param_value <- function(tbl, param = "alpha", id = 1) {
+    tbl %>%
+      filter(bandit == paste0(param, "_", id)) %>%
+      pull(value)
+  }
+  
+  plot_title <- paste0(
+    "Parameter Tracing Plot of Location: ",
+    bandit
+  )
+  
+  result <- param_logs$alpha %>%
+    rename(alpha = param) %>%
+    left_join(
+      param_logs$beta %>%
+        rename(beta = param),
+      by = "step"
+    ) %>%
+    mutate(
+      alpha = map_dbl(alpha, get_param_value, 
+                      param = "alpha", id = bandit),
+      beta = map_dbl(beta, get_param_value, 
+                      param = "beta", id = bandit),
+      theta = alpha/(alpha + beta)
+    ) %>%
+    ggplot(aes(x = step, y = theta)) +
+    geom_line(color = "darkred") + 
+    labs(y = "Positivity Rate",
+         title = plot_title) +
+    theme_minimal()
+  
+  return(result)
+}
 
 
 
